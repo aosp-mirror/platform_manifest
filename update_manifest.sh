zyx610402220623-aosp/manifest

initialize aosp main-cuttlefish-vendor-dev
Bug: 264504969
Test: repo sync
Change-Id: Ia176a95ab9382bbd917fe6b5960105631bed6f9f

update_manifest.sh
@@ -1,0 +1,5 @@
+#!/bin/bash
+
+./sync_manifest.sh
+./append_manifest.sh
+./exclude_projects.py

aosp-mirror/platform_manifest

update_manifest.sh
@@ -1,0 +1,5 @@
+#!/bin/bash
+
+./__sync_manifest.sh
+./__append_manifest.sh
+./__exclude_projects.py